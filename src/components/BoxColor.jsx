2c451eff2432fe9373e912e67816598dacc0faae
AdniSosa <adni.sori@gmail.com>
2025-03-28 12:15:16 +0100

    Terminado

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -1,7 +1,36 @@
-function boxColor ({color, value}) {
+//- `BoxColor.jsx`: Aquí irá el componente del cuadrado de color.
+
+
+import { useEffect, useRef, useState } from "react";
+
+function BoxColor({ color, value, input }) {
+  const [newValue, setNewValue] = useState('');
+  const [classList, setClassList] = useState('');
+  const divRef = useRef();
+
+  const colors = ['red', 'green', 'pink', 'yellow', 'purple', 'white', 'blue', 'aqua', 'olive'];
+  
+  const handleChange = (divColor) => {
+    if (color === divColor) {
+      //console.log(divRef.current.style.backgroundColor)
+    //return divRef.current.style.backgroundColor = divColor;
+    }
+  }
+
   return (
-    <>
-    </> 
+    <div className="container">
+      {colors.map((colorBox, index) => (
+        <div
+          key={index}
+          className={`box ${colorBox}`}
+          //ref={divRef}
+          //onChange={handleChange(`${colorBox}`)}
+          style={color === colorBox ? { backgroundColor: color, color: 'black' } : {}}
+        >
+          {color === colorBox ? value : `I'm not a ${color} color`}
+        </div>
+      ))}
+    </div>
   )
 }
-export default boxColor;  
+export default BoxColor;  
--- src/components/MyFormChallenge.jsx
+++ src/components/MyFormChallenge.jsx
@@ -1,9 +1,50 @@
+//- `MyFormChallenge.jsx`: Aquí crearemos el formulario e importaremos las BoxColor para pasarle `props`.
+//También tendremos un input en el que escribiremos el color para hacer que coincida.
+// El funcionamiento es el siguiente:
+// - Si escribimos un color que está en la lista, el cuadro de ese color tendra el fondo de ese color.
+// - Si el color (palabra que se escribe en el input) no coincide con el color, no cambiara ningun fondo.
+// - Mientras se escribe el color, ese valor del input irá cambiando en el texto dentro de cada cuadro.
+// - Tendremos 2 textos: Uno que diga que no soy el color y cambiará el texto cuando coincida el color.
+//const colors = ['red', 'green', 'pink', 'yellow', 'purple', 'white', 'blue', 'aqua', 'olive'];
 
+import { useState, useRef, useEffect } from "react";
+import BoxColor from "./BoxColor";
 
 function MyForm() {
 
+  const [value, setValue] = useState('');
+  const [inputText, setInputText] = useState('');
+  const [color, setColor] = useState('');
+  const inputRef = useRef(null)
+
+  const findColor = (inputText) => {
+    const colors = ['red', 'green', 'pink', 'yellow', 'purple', 'white', 'blue', 'aqua', 'olive'];
+    const colorFound = colors.find(color => inputText === color);
+
+    if (!colorFound) {
+      setColor(inputText);
+      return colorFound
+    }
+    setColor(colorFound);
+    setValue(`Yes, I'm ${colorFound} color`)
+  }
+
+  useEffect(() => {
+    findColor(inputText)
+  }, [inputText])
+
+
   return (
-    <></>
+    <>
+      <input
+        type="text"
+        value={inputText}
+        onChange={() => setInputText(inputRef.current.value)}
+        placeholder='Choose a color'
+        ref={inputRef}
+      />
+      <BoxColor color={color} value={value} />
+    </>
   );
 }
 
